af23b59e68421331980a94c9149e4600ee90c661
Your Name <ashihara@MacBook-Pro.local>
2020-12-31 15:55:14 +0900

    add あけおめ自動ツイート

---
+++ auto_tweet_new_year.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from twitter import Twitter, OAuth
+from datetime import datetime
+import os
+
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv("API_SECRET")
+
+t = Twitter(auth=OAuth(access_token, access_token_secret, api_key, api_secret))
+
+def main_process():
+    now_time = datetime.now().strftime("%Y年%m月%d日 %H時%M分%S秒")
+    tweet_text = f"【定期実行】\n" \
+                 f"{now_time}です。\n" \
+                 f"あけましておめでとうございます。\n" \
+                 f"本年もどうぞよろしくお願いします。"
+    t.statuses.update(status=tweet_text)
+
+def main():
+    main_process()
+
+if __name__ == '__main__':
+    main()
